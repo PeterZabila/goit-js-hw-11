0fad0e5cef0b503320c105976cf4cb5cf1cb9d09
Peter Zabila <petrozabila@gmail.com>
2022-06-17 00:52:38 +0300

    start

--- src/index.js
+++ src/index.js
@@ -5,84 +5,76 @@
 
 // const axios = require('axios').default;
 
-
-
-// 
-// async function getPic() {
-//   const query = document.querySelector('.searchInput').value;
-//     try {
-//       const response = await axios.get(`https://pixabay.com/api/?key=28076639-0feb76057bbd5c0e620bbf417&q=${query}&page=1&per_page=20`);
-//       console.log(response);
-//     } catch (error) {
-//       console.error(error);
-//     }
-//   }
-
-
-
-// const fetchPostsBtn = document.querySelector("#searchBtn");
-
-// const galleryEl = document.querySelector(".gallery");
-
-// fetchPostsBtn.addEventListener("click", () => {
-//   fetchPosts()
-//     .then((posts) => renderPosts(posts))
-//     .catch((error) => console.log(error));
-// });
-
-// ===================================================================================================
-// const form = document.querySelector('.search-form');
-// const searchBtn = document.querySelector('#searchBtn')
-
-// form.addEventListener('submit', fetchPosts);
-
-
-// function fetchPosts() {
-//   const query = document.querySelector('[name="searchQuery"]').value;
-//   const url = 'https://pixabay.com/api/';
-
-//   return fetch(`https://pixabay.com/api/?key=28076639-0feb76057bbd5c0e620bbf417&q=${query}&page=1&per_page=20`).then(
-//     // return fetch(url, params)
-  
-//       (response) => {
-//         if (!response.ok) {
-//           throw new Error(response.status);
-//         }
-//         console.log("Found");
-//         return response.json();
-//       }
-//     );
-//   }
-// ======================================================================================================
-
-  // const params = new URLSearchParams({
-  //   page: 1,
-  //   q: query,
-  //   per_page: 20,
-  //   key: 280766390-feb76057bbd5c0e620bbf417,
-  //   headers: {
-  //     'Content-Type': 'application/json'
-  //   },
-  // });
-
-
-  
-  // addGalleryItems(galleryItems);
-  
-  // new SimpleLightbox('.gallery a', { fadeSpeed: 500, captionDelay: 250, captionsData: "alt", scrollZoom: true, });
-  
-  
-// }
-
-
-import { searchImages } from 'pixabay-api';
-
 const refs = {
   form: document.querySelector('#search-form'),
   searchBtn: document.querySelector('#searchBtn'),
   gallery: document.querySelector('.gallery'),
   loadMoreBtn: document.querySelector('[data-action="load-more"]')
 }
+
+
+// const query = document.querySelector('.searchInput').value;
+// let page = 1;
+const BASE_URL = 'https://pixabay.com/api/';
+const API_KEY = '28076639-0feb76057bbd5c0e620bbf417';
+
+
+refs.form.addEventListener('submit', fetchPictures);
+
+   function fetchPictures(e) {
+    e.preventDefault();
+    const result = await axios.get(`${BASE_URL}`, {
+      params: {
+        key: API_KEY,
+        q: e.target.value,
+        image_type: "photo",
+        orientation: "horizontal",
+        safesearch: true,
+      }
+    })
+        
+    console.log(result);
+  }
+
+    //     return fetch(url, options)
+    //         .then(r => r.json())
+    //         .then(data => {
+    //             this.incrementPage();
+    //             return data.json();
+    //         })
+    //         .then(this.renderMarkup(pictures))
+    //         .catch(error => console.log(error))       
+    // }
+
+function createGalleryItemMarkup(hits) {
+  return hits.map(({webformatURL, largeImageURL, tags, likes, views, comments, downloads}) => {
+    const card = document.createElement('div');
+    card.classList.add('.photo-card');
+    return card.innerHTML = `
+    <div class="img-wrapper"><img class="img" src="${largeImageURL}", alt="${tags}" /></div>
+    <div class="info">
+      <p class="info-item">
+        <b>Likes:<br>${likes}</b>
+      </p>
+      <p class="info-item">
+        <b>Likes:<br>${views}</b>
+      </p>
+      <p class="info-item">
+        <b>Likes:<br>${comments}</b>
+      </p>
+      <p class="info-item">
+        <b>Likes:<br>${downloads}</b>
+      </p>
+    </div>`;
+    
+}).join("");
+
+}
+
+import axios from 'axios';
+import { searchImages } from 'pixabay-api';
+
+
 
 const searchQuery = document.querySelector('.searchInput').value;
 const AUTH_KEY = '28076639-0feb76057bbd5c0e620bbf417';
--- src/picturesHandle.js
+++ src/picturesHandle.js
@@ -1,56 +1,51 @@
-const query = document.querySelector('.searchInput').value;
-let page = 1;
+// const query = document.querySelector('.searchInput').value;
+// let page = 1;
+// const BASE_URL = 'https://pixabay.com/api/';
 
 
+//    function fetchPictures(e) {
+//     e.preventDefault();
 
-const options = {
-    headers:{
-        key: '28076639-0feb76057bbd5c0e620bbf417',
-    }
-}
+//         const options = {
+//             headers:{
+//                 key: '28076639-0feb76057bbd5c0e620bbf417',
+//             }
+//         }
 
+//         const url = `https://pixabay.com/api?q=${query}&per_page=20&page=${this.page}`;
 
-   function fetchPictures() {
-        const options = {
-            headers:{
-                key: '28076639-0feb76057bbd5c0e620bbf417',
-            }
-        }
-
-        const url = `https://pixabay.com/api?q=${query}&per_page=20&page=${this.page}`;
-
-        return fetch(url, options)
-            .then(r => r.json())
-            .then(data => {
-                this.incrementPage();
-                return data.json();
-            })
-            .then(this.renderMarkup(pictures))
-            .catch(error => console.log(error))       
-    }
+//         return fetch(url, options)
+//             .then(r => r.json())
+//             .then(data => {
+//                 this.incrementPage();
+//                 return data.json();
+//             })
+//             .then(this.renderMarkup(pictures))
+//             .catch(error => console.log(error))       
+//     }
 
    
-   function renderMarkup(pictures) {
-            const markup = pictures.reduce(({webformatURL, largeImageURL, tags, likes, views, comments, downloads}) => {
-                    return acc + `
-                    <li class="gallery__item">
-                        <a class="gallery__item" href="${largeImageURL}">
-                            <img class="gallery__image" src="${webformatURL}" alt="${tags}" />
-                        </a>
-                    </li>`
-            }, '')
-        const list = document.querySelector('.pictures-list')
-        list.insertAdjacentHTML('afterbegin', markup);
-    };
+//    function renderMarkup(pictures) {
+//             const markup = pictures.reduce(({webformatURL, largeImageURL, tags, likes, views, comments, downloads}) => {
+//                     return acc + `
+//                     <li class="gallery__item">
+//                         <a class="gallery__item" href="${largeImageURL}">
+//                             <img class="gallery__image" src="${webformatURL}" alt="${tags}" />
+//                         </a>
+//                     </li>`
+//             }, '')
+//         const list = document.querySelector('.pictures-list')
+//         list.insertAdjacentHTML('afterbegin', markup);
+//     };
     
 
 
-function incrementPage() {
-        page += 1;
-    }
+// function incrementPage() {
+//         page += 1;
+//     }
 
-function resetPage() {
-        page = 1;
-}
+// function resetPage() {
+//         page = 1;
+// }
 
    
