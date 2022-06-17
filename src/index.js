92d892d8e584cd8d22e0b485d1c350745f055d85
ZABILA PETRO <petrozabila@gmail.com>
2022-06-17 16:48:32 +0300

    got pictures

--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@
 
 // const axios = require('axios').default;
 
+import axios from 'axios';
+
 const refs = {
   form: document.querySelector('#search-form'),
   searchBtn: document.querySelector('#searchBtn'),
@@ -21,30 +23,33 @@ const API_KEY = '28076639-0feb76057bbd5c0e620bbf417';
 
 refs.form.addEventListener('submit', fetchPictures);
 
-   function fetchPictures(e) {
+async function fetchPictures(e) {
     e.preventDefault();
-    const result = await axios.get(`${BASE_URL}`, {
-      params: {
-        key: API_KEY,
-        q: e.target.value,
-        image_type: "photo",
-        orientation: "horizontal",
-        safesearch: true,
-      }
-    })
-        
-    console.log(result);
-  }
 
-    //     return fetch(url, options)
-    //         .then(r => r.json())
-    //         .then(data => {
-    //             this.incrementPage();
-    //             return data.json();
-    //         })
-    //         .then(this.renderMarkup(pictures))
-    //         .catch(error => console.log(error))       
-    // }
+    try {
+      const result = await axios.get(`${BASE_URL}`, {
+        params: {
+          key: API_KEY,
+          q: e.target.value,
+          image_type: "photo",
+          orientation: "horizontal",
+          safesearch: true,
+        }
+
+      })     
+      console.log(result);
+      
+        const data = await result.data.hits;
+      console.log(data);
+      
+       const markup = await createGalleryItemMarkup(data);
+       refs.gallery.insertAdjacentHTML('afterbegin', markup);
+      //  refs.loadMoreBtn.classList.toggle("hidden");
+    } catch (error) {
+      console.log(error);
+    } 
+}
+
 
 function createGalleryItemMarkup(hits) {
   return hits.map(({webformatURL, largeImageURL, tags, likes, views, comments, downloads}) => {
@@ -71,58 +76,21 @@ function createGalleryItemMarkup(hits) {
 
 }
 
-import axios from 'axios';
-import { searchImages } from 'pixabay-api';
 
+// ====================================================
 
+// import { searchImages } from 'pixabay-api';
 
-const searchQuery = document.querySelector('.searchInput').value;
-const AUTH_KEY = '28076639-0feb76057bbd5c0e620bbf417';
+// searchImages(AUTH_KEY, "cat", options).then((r) => console.log(r));
 
-const options = {
-  q: searchQuery,
-  image_type: 'photo',
-  orientation: 'horizontal',
-  safesearch: true,
-  
-};
-
-searchImages(AUTH_KEY, "cat", options).then((r) => console.log(r));
-
-function fetchSearch() {
-  searchImages(AUTH_KEY, `${searchQuery}`, options).then((r) => console.log(r));
-}
-
-refs.searchBtn.addEventListener('click', fetchSearch)
-
-// const query = document.querySelector('.searchInput').value;
-// let page = 1;
-
-
-// const url = `https://pixabay.com/api/?key=${API_KEY}&q=${query}&per_page=20&page=1`;
-// const options = {
-//   headers:{
-//       key: '28076639-0feb76057bbd5c0e620bbf417',
-//   }
+// async function fetchSearch() {
+//  const pictures = await axios.get(searchImages(AUTH_KEY, `${searchQuery}`, options))
+//   console.log(pictures);
 // }
 
-// function handleSearchBtn() {
-//   e.preventDefault();
-//  return fetch(url, options)
-//     .then(r => r.json())
-//     .then(data => console.log(data))
-//     .catch(er => console.log(er))
-// }
+// refs.searchBtn.addEventListener('click', fetchSearch)
 
-
-// refs.form.addEventListener('submit', handleSearchBtn);
-
-
-
-
-
-// refs.form.addEventListener('submit', onSearch);
-// refs.loadMoreBtn.addEventListener('click', onLoadMore);
+// ====================================================
 
 // function onSearch(e) {
 //   e.preventDefault;
